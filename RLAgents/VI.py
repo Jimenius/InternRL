f6e797e133e9af4663980be0fcea25b861606365
Jimenius <l.minhui@qq.com>
2019-07-06 20:23:12 +0800

    #2, VI enabled

--- RLAgents/VI.py
+++ RLAgents/VI.py
@@ -19,9 +19,10 @@ class ValueIterationAgent(Agent):
         # Initialize the agent
         try:
             self.load_brain(models)
-            self.policy = self._extract_policy()
+            self._extract_policy()
         except:
-            self.V = np.random.normal(0, 1, size = self.state_dim) # State values
+            self.V = np.zeros(self.state_dim)
+            #self.V = np.random.normal(0, 1, size = self.state_dim) # State values
             self.policy = np.zeros(self.state_dim, dtype = np.int32) # Current policy
 
     def _extract_policy(self):
@@ -29,7 +30,7 @@ class ValueIterationAgent(Agent):
         Description:
             Policy extraction bound method, extract policy from state values
         '''
-        
+
         for state in range(self.state_dim):
             p = 0
             newV = float('-inf')
@@ -43,8 +44,6 @@ class ValueIterationAgent(Agent):
                     p = action
 
             self.policy[state] = p
-        
-        return self.policy
 
     def learn(self, max_epoch = 1000, eval = False, logger = None):
         '''
@@ -55,9 +54,9 @@ class ValueIterationAgent(Agent):
         max_epoch: int
             Maximum learning epochs
         '''
-        
+
         for _ in range(max_epoch):
-            delta = self.theta
+            delta = 0
             for state in range(self.state_dim):
                 oldV = self.V[state]
                 newV = float('-inf')
@@ -66,9 +65,9 @@ class ValueIterationAgent(Agent):
                     v = 0
                     for trans_prob, next_state, reward, _ in transition:
                         v += trans_prob * (reward + self.gamma * self.V[next_state])
-
-                if v > newV:
-                    newV = v
+                    
+                    if v > newV:
+                        newV = v
                     
                 self.V[state] = newV
                 delta = max(delta, abs(oldV - newV))
@@ -78,8 +77,7 @@ class ValueIterationAgent(Agent):
             if eval:
                 _ = self.render(num_episode = 1, vis = False, intv = 0, logger = logger)
             
-        _ = self._extract_policy()
-        return self.V
+        self._extract_policy()
 
     def load_brain(self, models):
         self.V = np.load('models/VI/' + models[0] + '.npy')
--- scripts/main.py
+++ scripts/main.py
@@ -42,5 +42,22 @@ if __name__ == '__main__':
         if opt.LOGDIR and opt.EVAL and opt.PLOT: # Plot can only be drawn when learning is evaluated and logged.
             main_logger('Plot episodic reward of learning process...')
             plot_learn(logdir + 'PILearn{}.log'.format(tic))
+    elif agent_name == 'VALUEITERATION':
+        agent = RLAgents.VI.ValueIterationAgent(env = env, theta = opt.THETA, gamma = opt.GAMMA)
+        learn_logger = Logger('MainLearn', logdir = logdir, logfile = 'VILearn{}.log'.format(tic))
+        main_logger('Start learning...')
+        agent.learn(max_epoch = opt.MAX_EPOCH, eval = opt.EVAL, logger = learn_logger)
+        toc = int(time())
+        main_logger('Learning takes {}s'.format(toc - tic))
+        main_logger('Start evaluation...')
+        _ = agent.render(num_episode = opt.NUM_EPISODE, vis = opt.VIS, intv = opt.INTERVAL, logger = main_logger)
+        if opt.MEMORIZE:
+            main_logger('Saving learned models...')
+            agent.save_brain(tic)
+        if opt.LOGDIR and opt.EVAL and opt.PLOT: # Plot can only be drawn when learning is evaluated and logged.
+            main_logger('Plot episodic reward of learning process...')
+            plot_learn(logdir + 'VILearn{}.log'.format(tic))
+    else:
+        main_logger('The agent is not supported at this moment.')
 
     main_logger('Program takes {}s'.format(toc - tic))
