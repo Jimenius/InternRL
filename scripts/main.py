7490603779a2c777afc6e79516716c1a284c6044
Jimenius <l.minhui@qq.com>
2019-07-07 15:26:09 +0800

    #6, Put redundant parts together

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,7 @@ from utils.utils import ParseArguments, plot_learn
 from utils.log import Logger
 
 if __name__ == '__main__':
-
+    agent_set = set(('POLICYITERATION', 'VALUEITERATION', 'SARSA', 'QLEARNING'))
     tic = int(time()) # Identify the program process by the timestamp
     
     opt = ParseArguments()    
@@ -29,52 +29,21 @@ if __name__ == '__main__':
 
     if agent_name == 'POLICYITERATION':
         agent = RLAgents.PI.PolicyIterationAgent(env = env, theta = opt.THETA, gamma = opt.GAMMA)
-        learn_logger = Logger('MainLearn', logdir = logdir, logfile = 'PILearn{}.log'.format(tic))
-        main_logger('Start learning...')
-        agent.learn(max_epoch = opt.MAX_EPOCH, eval = opt.EVAL, logger = learn_logger)
-        toc = int(time())
-        main_logger('Learning takes {}s'.format(toc - tic))
-        main_logger('Start evaluation...')
-        _ = agent.render(num_episode = opt.NUM_EPISODE, vis = opt.VIS, intv = opt.INTERVAL, logger = main_logger)
-        if opt.MEMORIZE:
-            main_logger('Saving learned models...')
-            agent.save_brain(tic)
-        if opt.LOGDIR and opt.EVAL and opt.PLOT: # Plot can only be drawn when learning is evaluated and logged.
-            main_logger('Plot episodic reward of learning process...')
-            plot_learn(logdir + 'PILearn{}.log'.format(tic))
+        logfile = 'PILearn{}.log'.format(tic)
     elif agent_name == 'VALUEITERATION':
         agent = RLAgents.VI.ValueIterationAgent(env = env, theta = opt.THETA, gamma = opt.GAMMA)
-        learn_logger = Logger('MainLearn', logdir = logdir, logfile = 'VILearn{}.log'.format(tic))
-        main_logger('Start learning...')
-        agent.learn(max_epoch = opt.MAX_EPOCH, eval = opt.EVAL, logger = learn_logger)
-        toc = int(time())
-        main_logger('Learning takes {}s'.format(toc - tic))
-        main_logger('Start evaluation...')
-        _ = agent.render(num_episode = opt.NUM_EPISODE, vis = opt.VIS, intv = opt.INTERVAL, logger = main_logger)
-        if opt.MEMORIZE:
-            main_logger('Saving learned models...')
-            agent.save_brain(tic)
-        if opt.LOGDIR and opt.EVAL and opt.PLOT: # Plot can only be drawn when learning is evaluated and logged.
-            main_logger('Plot episodic reward of learning process...')
-            plot_learn(logdir + 'VILearn{}.log'.format(tic))
+        logfile = 'VILearn{}.log'.format(tic)
     elif agent_name == 'SARSA':
         agent = RLAgents.SARSA.SARSAAgent(env = env, epsilon = opt.EPSILON, lr = opt.LR, gamma = opt.GAMMA)
-        learn_logger = Logger('MainLearn', logdir = logdir, logfile = 'SARSALearn{}.log'.format(tic))
-        main_logger('Start learning...')
-        agent.learn(max_epoch = opt.MAX_EPOCH, eval = opt.EVAL, logger = learn_logger)
-        toc = int(time())
-        main_logger('Learning takes {}s'.format(toc - tic))
-        main_logger('Start evaluation...')
-        _ = agent.render(num_episode = opt.NUM_EPISODE, vis = opt.VIS, intv = opt.INTERVAL, logger = main_logger)
-        if opt.MEMORIZE:
-            main_logger('Saving learned models...')
-            agent.save_brain(tic)
-        if opt.LOGDIR and opt.EVAL and opt.PLOT: # Plot can only be drawn when learning is evaluated and logged.
-            main_logger('Plot episodic reward of learning process...')
-            plot_learn(logdir + 'SARSALearn{}.log'.format(tic))
+        logfile = 'SARSALearn{}.log'.format(tic)
     elif agent_name == 'QLEARNING':
         agent = RLAgents.QL.QLearningAgent(env = env, epsilon = opt.EPSILON, lr = opt.LR, gamma = opt.GAMMA)
-        learn_logger = Logger('MainLearn', logdir = logdir, logfile = 'QLLearn{}.log'.format(tic))
+        logfile = 'SARSALearn{}.log'.format(tic)
+    else:
+        main_logger('The agent is not supported at this moment.')
+
+    if agent_name in agent_set:
+        learn_logger = Logger('MainLearn', logdir = logdir, logfile = logfile)
         main_logger('Start learning...')
         agent.learn(max_epoch = opt.MAX_EPOCH, eval = opt.EVAL, logger = learn_logger)
         toc = int(time())
@@ -86,8 +55,6 @@ if __name__ == '__main__':
             agent.save_brain(tic)
         if opt.LOGDIR and opt.EVAL and opt.PLOT: # Plot can only be drawn when learning is evaluated and logged.
             main_logger('Plot episodic reward of learning process...')
-            plot_learn(logdir + 'QLLearn{}.log'.format(tic))
-    else:
-        main_logger('The agent is not supported at this moment.')
+            plot_learn(logdir + logfile)
 
     main_logger('Program takes {}s'.format(toc - tic))
